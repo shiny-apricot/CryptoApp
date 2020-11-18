bd63e7a1a6f6886cc83a0bc8d02a7abdc43fe189
shiny-apricot <yasininal44@gmail.com>
2020-11-18 22:47:02 +0300

    genel

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lab_4/pages/favorites.dart';
+import 'package:lab_4/pages/login.dart';
+import 'package:lab_4/pages/main_page.dart';
+
+
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: 200,
-          height: 200,
-          child: ListTile(
-            tileColor: Colors.grey,
-            leading: Icon(
-              Icons.access_time,
-              color: Colors.blue,
-            ),
-          )
-         )
-        ),
-      )
-    );
-  }
+  runApp(Favorites());
 }
--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,1 +1,13 @@
-//deneme yazisi favorites
+import 'package:flutter/material.dart';
+
+class Favorites extends StatefulWidget {
+  @override
+  _FavoritesState createState() => _FavoritesState();
+}
+
+class _FavoritesState extends State<Favorites> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
