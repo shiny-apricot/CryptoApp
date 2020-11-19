52a8020ee42b39b82dde0ff86f8f030c59a730ad
shiny-apricot <yasininal44@gmail.com>
2020-11-20 01:33:28 +0300

    investment page and currency data

---
+++ lib/data/currencyValues.dart
@@ -0,0 +1,5 @@
+class CurrencyValues{
+  static double TRY = 0.24;
+  static double BTC = 35214;
+  static double ETH = 473;
+}
---
+++ lib/data/investment.dart
@@ -0,0 +1,9 @@
+class Investment {
+  double count;
+  double initialValue;
+  String type;
+  String valueType;
+
+  Investment({this.count, this.initialValue, this.type, this.valueType});
+
+}
