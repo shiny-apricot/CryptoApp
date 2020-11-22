9b8b89fed2f3cb1f6af51a1389861799a6eda0d6
yusufuysal <74680956+yusufuysal@users.noreply.github.com>
2020-11-22 18:38:47 +0300

    yusuf

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../main.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -8,6 +11,251 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      resizeToAvoidBottomInset: false,
+
+        body: Container(
+
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.lightBlue[700],
+                Colors.cyan[400],
+                Colors.cyan[300],
+              ]
+            ),
+          ),
+
+                 child:Column(
+
+                   children: <Widget>[
+                     SizedBox(
+                       height: 300,
+                     ),
+                     Align(
+                     child:Container(
+                       height: 50,
+                       width: 250,
+
+                       margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                       decoration: new BoxDecoration(
+                         color: Color(0xFFAED6F1 ),
+                         borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
+                       ),
+                       child: Padding(
+                         padding: const EdgeInsets.only(left: 20,top: 20),
+                          child: TextField(
+                           decoration: InputDecoration.collapsed(
+                             hintStyle: TextStyle(
+                                 color: Colors.black26,
+                             ),
+                             hintText: 'Username',
+                             border: InputBorder.none,
+                           ),
+                         ),
+                       ),
+                      ),
+                     ),
+
+
+                    Align(
+                      child: Container(
+                       height: 50,
+                       width: 250,
+                       margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                       decoration: new BoxDecoration(
+                         color: Color(0xFFAED6F1 ),
+                         borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
+                       ),
+                       child: Padding(
+                         padding: const EdgeInsets.only(left: 20),
+                          child: TextField(
+                           decoration: InputDecoration(
+                             hintStyle: TextStyle(
+                               color: Colors.black26,
+                             ),
+                             hintText: 'Password',
+                             border: InputBorder.none,
+                           ),
+                         ),
+                       ),
+                     ),
+                    ),
+                     SizedBox(
+                       height: 10,
+                     ),
+                     Padding(
+                       padding: const EdgeInsets.only(left: 160.0),
+                       child: TextButton(
+                         onPressed: (){
+
+                         },
+                         child: Text(
+                           'Remember me',
+                           style: TextStyle(
+                             color: Colors.black,
+
+                           ),
+                         ),
+                       ),
+                     ),
+                     SizedBox(
+                       height: 1,
+                     ),
+                     Align(
+                      child:Container(
+                        height: 50,
+                        width: 180,
+
+                        margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                        decoration: new BoxDecoration(
+                          color: Color(0xFFE19600),
+                          borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
+                        ),
+                       child: FlatButton(
+                          child: Text('LOG IN',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                              ),
+                          ),
+                        onPressed: (){
+                          Navigator.push(context, new MaterialPageRoute(
+                          builder: (context) => Home(),
+                            ));
+                          },
+                        ),
+                      ),
+                     ),
+
+                  SizedBox(
+                    height: 160,
+                  ),
+                 Expanded(
+
+                   child: Container(
+                      width: double.infinity,
+                        height: 100,
+                         color: Color(0xFF01ACC4),
+                          child: Row(
+                            children:<Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 50),
+
+                                  child:Text(
+                                    "Don't you have an account?",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+
+
+                              ),
+                              Expanded(
+                                child:TextButton(
+                                  child: Text(
+                                    'Sign up!',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]
+
+
+
+                     ),
+
+
+                   ),
+                 ),
+
+                   ],
+
+                 ),
+
+                            /*child: Container(
+                              height: 50,
+                              width: 250,
+                              color: Colors.greenAccent,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                  hintStyle: TextStyle(
+                                  color: Colors.blue
+                                  ),
+                                  hintText: 'Name',
+                                ),
+                               ),
+                             ),
+                          ),
+
+                        Container(
+                        height: 50,
+                        width: 250,
+                        color: Colors.greenAccent,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(
+                                color: Colors.blue
+                            ),
+                            hintText: 'Name',
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 250,
+                        color: Colors.greenAccent,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(
+                                color: Colors.blue
+                            ),
+                            hintText: 'Password',
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 200,
+                        color: Colors.amber,
+                        child: FlatButton(
+
+                          child: Text('Login',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          onPressed: (){
+                            Navigator.push(context, new MaterialPageRoute(
+                              builder: (context) => Home(),
+                            ));
+                          },
+                        ),
+                ),*/
+
+
+
+
+
+
+
+
+
+          ),
+
+    );
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,21 +1,305 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class MainPage extends StatefulWidget {
+
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  bool isPressed = false;
+  bool isPressed1 = false;
+  bool isPressed2 = false;
+  bool isPressed3 = false;
+  bool isPressed4 = false;
+  bool isPressed5 = false;
+  bool isPressed6 = false;
   @override
   Widget build(BuildContext context) {
+
     return SafeArea(
       child: Column(
         children: [
+          Align(
+            child: Container(
+              height: 50,
+              width: 300,
+              margin: EdgeInsets.only(top: 40, left: 40, right: 40),
+              decoration: new BoxDecoration(
+                color: Color(0xFF003941),
+                borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
+                border: Border.all(color: Colors.white, width: 0.0),
+              ),
+                child: ListTile(
+                  trailing: Icon(FontAwesomeIcons.liraSign,
+                  color: Colors.red,
+                  ),
+                  title: Text('Selected Currency',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                )
+            ),
+          ),
+
+          Align(
+            child: Container(
+              margin: EdgeInsets.only(top: 40, left: 40, right: 40),
+
+              decoration: new BoxDecoration(
+
+                color: Color(0xFF003941),
+                borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
+                border: Border.all(color: Colors.white, width: 0.0),
+
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                  child: TextField(
+                   decoration: InputDecoration(
+
+                    hintStyle: TextStyle(
+                        color: Colors.tealAccent[100],
+                      fontSize: 15,
+                      fontWeight: FontWeight.w100,
+                    ),
+                    hintText: 'Search',
+                    border:InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height:20,
+          ),
           Expanded(
+
             child: Container(
               color: Color(0xFF003942),
+              child: GestureDetector (child:ListView(
+                scrollDirection: Axis.vertical,
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
 
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed == true){
+                            setState(() {
+                              isPressed = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.coins,
+                            color: Colors.orange,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                        color: (isPressed) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('Bitcoin              139,147.48',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
 
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed1 == true){
+                            setState(() {
+                              isPressed1 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed1 = true;
+                            });
+                          }
+                        },
+
+                        leading: Icon(FontAwesomeIcons.coins,
+                          color: Colors.orange
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed1) ? Colors.red : Colors.white,
+                        ),
+
+                        title: Text('Etherium',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed2 == true){
+                            setState(() {
+                              isPressed2 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed2 = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.dollarSign,
+                        color: Colors.green,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed2) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('Dollar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed3 == true){
+                            setState(() {
+                              isPressed3 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed3 = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.euroSign,
+                            color: Colors.green,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed3) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('Euro',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed4 == true){
+                            setState(() {
+                              isPressed4 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed4 = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.coins,
+                            color: Colors.orange,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed4) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('ChainLink',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed5 == true){
+                            setState(() {
+                              isPressed5 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed5 = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.coins,
+                            color: Colors.orange,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed5) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('Litecoin',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color(0xFF003942),
+                      child: ListTile(
+                        onTap: (){
+                          if(isPressed6 == true){
+                            setState(() {
+                              isPressed6 = false;
+                            });
+                          } else {
+                            setState(() {
+                              isPressed6 = true;
+                            });
+                          }
+                        },
+                        leading: Icon(FontAwesomeIcons.coins,
+                            color: Colors.orange,
+                        ),
+                        trailing: Icon(FontAwesomeIcons.solidHeart,
+                          color: (isPressed6) ? Colors.red : Colors.white,
+                        ),
+                        title: Text('Bitcoin Cash',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      )
+                  ),
+
+                ],
+              ),
+
+              ),
 
               /*KODU BURAYA YAZIN*/
 
@@ -23,6 +307,9 @@ class _MainPageState extends State<MainPage> {
 
             ),
           ),
+            SizedBox(
+              height: 40,
+            ),
           Container(
             alignment: Alignment.bottomCenter,
             child: Divider(
