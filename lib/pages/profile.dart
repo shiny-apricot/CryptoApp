4c19c50337001d604f8fb35a1f2cc8125cb81c1e
eminsahin2344 <74674317+eminsahin2344@users.noreply.github.com>
2020-11-28 15:23:04 +0300

    emin
    bayrak

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -14,17 +14,19 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return
       Container(
-      margin: EdgeInsets.fromLTRB(20,60,0,0),
+      margin: EdgeInsets.fromLTRB(0,60,0,0),
         child: Column(
           children: [
-            Container(
-              child: Text('Yasin İNAL',
-                style:TextStyle(color: Colors.white, fontSize: 30.0))
+            Center(
+              child: Container(
+                child: Text('Yasin İNAL',
+                  style:TextStyle(color: Colors.white, fontSize: 50.0))
+              ),
             )
             ,
             Container(
               margin: EdgeInsets.fromLTRB(0, 80, 0, 0),
-              child: Text('Current Currency',style:TextStyle(color: Colors.white, fontSize: 20.0,fontStyle: FontStyle.italic)
+              child: Text('Current Currency',style:TextStyle(color: Colors.white, fontSize: 25.0,fontStyle: FontStyle.italic)
               ),
             ),
             Container(
@@ -37,79 +39,50 @@ class _ProfileState extends State<Profile> {
               height: 50,
               margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
               child: Center(
-                child: DropdownButton<String>(
-                  dropdownColor: Colors.black,
-                  value: Currency,
-                  icon: Icon(Icons.arrow_downward,
-                    color: Colors.white,),
-                  iconSize: 24,
-                  elevation: 16,
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      Currency = newValue;
-                    });
-                  },
-                  items: <String>['TRY', 'USD']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
+                child: Row(
+                  children: [
+                    Container(
+                      padding:EdgeInsets.only(left: 5),
+                      child: DropdownButton<String>(
+                        dropdownColor: Color(0xFF002B32),
+                        value: Currency,
+                        icon: Icon(Icons.arrow_downward,
+                          color: Colors.white,),
+                        iconSize: 24,
+                        style: TextStyle(
+                          fontSize: 25,
+                            color: Colors.white
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            Currency = newValue;
+                          });
+                        },
+                        items: <String>['TRY', 'USD','ETH','BTC']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        })
+                            .toList(),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Container(
+                        width: 30,
+                        height: 30,
+
+                        child: Image(
+                          image: AssetImage('assets/turkishflag.jpeg'),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
-            Container(
-            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-    child: Text('Language',style:TextStyle(color: Colors.white, fontSize: 20.0,fontStyle: FontStyle.italic)
-    ),
-    ),
-            Container(
-              decoration: new BoxDecoration(
-                color: Color(0x20D3D3D3),
-                borderRadius: new BorderRadius.all(Radius.elliptical(20, 50)),
-                border: Border.all(color: Colors.white, width: 0.0),
-              ),
-              width: 150,
-              height: 50,
-              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-              child: Center(
-                child: DropdownButton<String>(
-                  
-                  dropdownColor:Colors.black ,
-                  value: Language,
-                  icon: Icon(Icons.arrow_downward,
-                  color: Colors.white,),
-                  iconSize: 24,
-                  elevation: 16,
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      Language = newValue;
-                    });
-                  },
-                  items: <String>['English', 'Türkçe']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-              ),
-
-
-              ),
-
-
-
           ],
 
 
